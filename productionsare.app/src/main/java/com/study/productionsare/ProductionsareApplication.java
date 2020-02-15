25e3db0dc2b0afbc0917297b27acb1b339f0c3fd
JeMor <1839750567@qq.com>
2020-02-15 19:20:48 +0800

    初始项目

---
+++ productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
@@ -0,0 +1,13 @@
+package com.study.productionsare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionsareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionsareApplication.class, args);
+    }
+
+}
---
+++ productionsare.app/src/test/java/com/study/productionsare/ProductionsareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.productionsare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductionsareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
