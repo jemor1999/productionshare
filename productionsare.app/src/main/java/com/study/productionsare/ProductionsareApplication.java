b60e37c098ce8b51b0d455782826da925c544807
JeMor <1839750567@qq.com>
2020-02-17 13:35:38 +0800

    添加了数据库配置

--- productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
+++ productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
@@ -3,11 +3,14 @@ package com.study.productionsare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class ProductionsareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductionsareApplication.class, args);
+        System.out.println("hello----------");
     }
 
 }
