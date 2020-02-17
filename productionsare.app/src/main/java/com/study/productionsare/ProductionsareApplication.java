2c30ee4d38ac743758d41478ac87e5c98979a1f8
ryk <1545056924@qq.com>
2020-02-17 13:40:50 +0800

    Commit Message

--- productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
+++ productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
@@ -8,6 +8,6 @@ public class ProductionsareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductionsareApplication.class, args);
-    }
 
+    }
 }
---
+++ productionshare.web/src/components/login.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <b-input-group prepend="账号" class="mt-3">
+    <b-form-input></b-form-input>
+  </b-input-group>
+  <b-input-group prepend="密码" class="mt-3">
+    <b-form-input></b-form-input>
+  </b-input-group>
+  <b-input-group-append>
+    <b-button variant="outline-success">Button</b-button>
+    <b-button variant="info">Button</b-button>
+  </b-input-group-append>
+</div>
+</template>
+
+<script>
+  export default {
+    name: "${COMPONENT_NAME}"
+  }
+</script>
+
+<style scoped>
+
+</style>
