31269f2a07f25da58d54d9e3c8fbc1ca7ca95d74
JeMor <1839750567@qq.com>
2020-02-18 11:05:11 +0800

    配置了注解扫描 写了一个service的例子

--- productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
+++ productionsare.app/src/main/java/com/study/productionsare/ProductionsareApplication.java
@@ -2,7 +2,9 @@ package com.study.productionsare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.study")
 @SpringBootApplication
 public class ProductionsareApplication {
 
