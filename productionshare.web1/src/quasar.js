c0ba6d9ff91708a2bcbf2174ba73db3c796b24a4
JeMor <1839750567@qq.com>
2020-02-19 22:01:59 +0800

    修bug中

--- productionshare.web1/src/quasar.js
+++ productionshare.web1/src/quasar.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 
 import 'quasar/dist/quasar.css'
 import 'quasar/dist/quasar.ie.polyfills'
-import lang from 'quasar/lang/zh-hans'
+import lang from 'quasar/lang/zh-hans.js'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
@@ -12,12 +12,42 @@ import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
 import '@quasar/extras/ionicons-v4/ionicons-v4.css'
 import '@quasar/extras/mdi-v4/mdi-v4.css'
 import '@quasar/extras/eva-icons/eva-icons.css'
-import { Quasar } from 'quasar'
+import {
+  Quasar, 
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar'
 
 Vue.use(Quasar, {
   config: {},
-  components: { /* not needed if importStrategy is not 'manual' */ },
-  directives: { /* not needed if importStrategy is not 'manual' */ },
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
   plugins: {
   },
   lang: lang
--- productionshare.web1/vue.config.js
+++ productionshare.web1/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pluginOptions: {
     quasar: {
-      importStrategy: 'kebab',
+      importStrategy: 'manual',
       rtlSupport: true
     }
   },
