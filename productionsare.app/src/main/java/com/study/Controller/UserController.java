85c305eb6f95a07563d463776f2228b8d981764a
JeMor <1839750567@qq.com>
2020-02-21 12:24:05 +0800

    变更了跨源处理方法

--- productionsare.app/src/main/java/com/study/Controller/UserController.java
+++ productionsare.app/src/main/java/com/study/Controller/UserController.java
@@ -4,22 +4,18 @@ import com.study.model.User;
 import com.study.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 
 
-@Controller
+@RestController
 public class UserController {
     @Autowired
     UserServiceImpl userService;
 
     @ResponseBody
-    @CrossOrigin//解析路由
     @RequestMapping("user/login")
     public User login(@RequestBody User user) throws NoSuchAlgorithmException {
           //将前端获取到得数据
@@ -28,8 +24,8 @@ public class UserController {
         User us=new User(username,userPwd);
         return userService.enterLoginUser(us);//返回user
     }
+
     @ResponseBody
-    @CrossOrigin//解析路由
     @RequestMapping("user/register")
   public User register(@RequestBody User user){
 //        String username=user.getUserAccount();
